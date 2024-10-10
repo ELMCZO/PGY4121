11d18ae0626b099531af808cfc99aecc45de117b
ELMCZO <127500160+ELMCZO@users.noreply.github.com>
2024-10-10 08:56:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PGY4121',
+  webDir: 'www'
+};
+
+export default config;
